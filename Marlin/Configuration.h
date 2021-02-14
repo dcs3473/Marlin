b7c4361b9109f6702006c07405a23a20e465eaef
David Stockdale <41482669+dcs3473@users.noreply.github.com>
2021-02-14 16:23:29 -0500

    Adjusting Probe

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1005,11 +1005,11 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -65, -30, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -65, -35, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 10
+#define PROBING_MARGIN 15
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_SPEED (266*60)
