6f2e43e3acab29df803221f64be457834763f71f
David Stockdale <41482669+dcs3473@users.noreply.github.com>
2021-02-24 08:02:23 -0500

    Tramming Turned On

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -816,11 +816,11 @@
 //
 // Add the G35 command to read bed corners to help adjust screws. Requires a bed probe.
 //
-//#define ASSISTED_TRAMMING
+#define ASSISTED_TRAMMING
 #if ENABLED(ASSISTED_TRAMMING)
 
   // Define positions for probe points.
-  #define TRAMMING_POINT_XY { {  20, 20 }, { 180,  20 }, { 180, 180 }, { 20, 180 } }
+  #define TRAMMING_POINT_XY { {  20, 20 }, { 150,  20 }, { 150, 180 }, { 20, 180 } }
 
   // Define position names for probe points.
   #define TRAMMING_POINT_NAME_1 "Front-Left"
